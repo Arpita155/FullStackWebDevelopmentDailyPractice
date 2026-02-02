398faad9b18372dc3d66a5eb25d22e683ba61a45
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-02-02 23:30:26 +0530

    msg

---
+++ Development/js/HomeWork/hw1.js
@@ -0,0 +1,155 @@
+//================================== switch case =======================================
+// Q1.Find day based on number.
+/*n = 2
+switch(n){
+    case 1:
+        console.log("Monday")
+        break
+    case 2:
+        console.log("Tuesday")
+        break  
+    case 3:
+        console.log("Wednesday")
+        break
+    case 4:
+        console.log("Thursday")
+        break
+    case 5:
+        console.log("Friday")
+        break  
+    case 6:
+        console.log("Saturday")
+        break 
+    case 7:
+        console.log("Sunday")
+        break  
+    default :
+    console.log("Invalid input")                     
+}
+*/
+
+// Q2.Basic calculator
+/*x=5
+y=10
+res = 0
+op = "a"
+switch(op){
+    case "+":
+        res = x+y
+        console.log(res)
+        break
+    case "-":
+        res = x-y
+        console.log(res)
+        break
+    case "/":
+        res = x/y
+        console.log(res)
+        break
+    case "*":
+        res = x*y
+        console.log(res)
+        break 
+    case "%":
+        res = x%y
+        console.log(res)
+        break 
+    default:
+        console.log("Invalid input")                  
+}
+*/
+
+// Q3. Give a Greeting
+/*g = 2
+switch(g){
+    case 1:
+        console.log("Good Morning")
+        break
+    case 2:
+        console.log("Good Noon")
+        break  
+    case 3:
+        console.log("Good afternoon")
+        break
+    case 4:
+        console.log("Good evening")
+        break
+    case 5:    
+        console.log("Good night")
+        break
+    default:
+        console.log("Invalid input")    
+}
+*/
+
+// Q4. Choose your Stream
+/*s = ""
+switch(s){
+    case "CSE":
+        console.log("Welcome to CSE")
+        break
+    case "ECE":
+        console.log("Welcome to ECE")
+        break  
+    case "":   
+        console.log("Empty stream , please give the value of the stream") 
+        break
+    default:
+        console.log("Wrong value")
+}
+*/
+
+
+
+//===================================== if else ==========================================
+//Q1. Grade system
+/*n = 87
+if(n>90){
+    console.log("A+")
+}else if(n<89 && n>80){
+    console.log("A")
+}else if(n<79 && n>70){
+    console.log("B")
+}else if(n<69 && n>60){
+    console.log("C")
+}else if(n<59 && n>50){
+    console.log("D")
+}else{
+    console.log("F")
+}
+*/
+
+// Q2. Course expenditure 
+/*course = prompt("Enter the course : ")
+if(course=="javascript"){
+    console.log("You have to pay 3000")
+}else if(course=="java"){
+    console.log("You have to pay 4000")
+}else if(course=="c++"){
+    console.log("You have to pay 2500")
+}else{
+    console.log("Not such course found")
+}
+*/
+
+// Q3. 
+/*a='b'
+console.log(typeof(a))
+if(typeof(a)=="number"){
+    console.log("a is a number")
+}else if(typeof(a)=="string"){
+    console.log("a is a string")
+}else if(typeof(a)=="boolean"){
+    console.log("a is a boolean")
+}else if(typeof(a)=="undefined"){
+    console.log("a is a undefined")
+}
+*/
+
+// Q4. Tell if String contains "js" print "js is there" or "js is not there"
+/*s = "arpitjsa"
+if(s.includes("js")){
+    console.log("js is there in the string")
+}else{
+    console.log("js is not there in the string")
+}*/
---
+++ Development/js/day2.js
@@ -0,0 +1,92 @@
+//=======================String====================
+x = "helloh"
+y = "world "
+
+z = x+y     // for concatenation
+console.log(z)   // helloworld
+l = x.length   // for checking length
+console.log(l)  // 5
+
+c = x.charAt(1);   // for char at particular index
+console.log(c);  // e
+
+s = x.slice(1,4)   // it will print between 1 to 3
+console.log(s)  // ell
+
+console.log(x.slice(-2))   // lo
+
+z = x.substring(1,4)
+console.log(z)  //slice and substring do same but in substring if you give -ve value then it treat the -ve value as 0 and print the whole string.
+
+z =x.toUpperCase()   // convert the string to capital letter.
+console.log(z)
+z =x.toLowerCase()   // convert the string to all small letter.
+console.log(z)
+
+z =y.trim()    // it will remove extra spaces from both first and last
+console.log(z)
+
+z =y.repeat(3)   // it repeats the same string to the given time(3) .
+console.log(z)
+ 
+z =x.replace("h","t")  // it will replace first "h" to "t"
+console.log(z)
+
+z =x.replaceAll("h","t")  // it will replace all "h" to "t"
+console.log(z)
+
+r= '12 25 20 23 45'
+z = r.split(" ")    // it split the string 
+console.log(z)
+
+z = x.indexOf("l")   // it returns the index of the letter.
+console.log(z)
+
+z = x.lastIndexOf("l")    // it returns the the last index of the specific letter from last.
+console.log(z)
+
+z = x.includes("i")    // it returns true or false if the letter present in the string or not.
+console.log(z)
+
+k = "cake, biscuit, apple , fish"    // convert a string to an array.
+z = k.split(",")
+console.log(z)
+
+console.log(typeof(12))   // it returns the type of the value.
+
+console.log(1=='1')   // checks equality by converting both operand into a common data type.
+console.log(1==='1')   // checks equality but without converting both operand into a common data type.
+
+
+
+// ===============if else=====================
+//Q1.
+a=120
+if(a>100){
+    console.log("greater than 100")
+}else{
+    console.log("less than 100")
+}
+
+//Q2.
+age = 20
+if(age < 10){
+    console.log("child")
+}else if(age > 18 && age < 30){
+    console.log("adult")
+}else{
+    console.log("old")
+}
+//====================== Switch case ===============
+b = 8
+switch(b){
+    case 1:
+        console.log("one")
+        break
+    case 2:
+        console.log("two")
+        break
+    default:
+        console.log("wrong value")    
+}
+
